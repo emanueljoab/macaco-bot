36cb6a8aff6fd284b52753aa190f9f8202809b20
Emanuel Joab <emanueljoab@msn.com>
2024-07-17 01:54:09 -0300

    add simp to help command

--- commands/simp.js
+++ commands/simp.js
@@ -3,11 +3,21 @@ const { EmbedBuilder } = require('discord.js');
 async function execute(message, __, __, translate) {
     const user = message.mentions.users.first() || message.author;
     const simp = Math.floor(Math.random() * 101);
+    let footer;
+
+    if (simp === 100) {
+        footer = 'GADO! 🐂'
+    }
 
     const embed = new EmbedBuilder()
         .setTitle(await translate('simp', 'setTitle'))
         .setDescription(await translate('simp', 'setDescription', user.username, simp))
         .setThumbnail('https://i.imgur.com/gvRF6X5.jpg')
+
+        if (footer) {
+        embed.setFooter( {text: footer} )
+        }
+
     await message.reply({ embeds: [embed] });
     console.log(`${new Date().toLocaleString('pt-BR')} | ${user.username} é ${simp}% simp (${message.author.username})`);
 }
